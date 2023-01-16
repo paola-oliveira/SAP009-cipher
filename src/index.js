9a3cede3b4d4a4b23c1ad6cf5336ad9ac49fb1e5
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-01-16 14:52:49 -0300

    Primeiro Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const offset = document.getElementById("offset");
+const mensagemOriginal = document.getElementById("original");
+const mensagemCifrada = document.getElementById("cifrada");
+
+const botãocodificar = document.getElementById("criptografar");
+const botãodescodificar = document.getElementById("descriptografar");
