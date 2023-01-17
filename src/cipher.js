b6232819055c52bb1b0e518326c485ca17ec412b
paola-oliveira <paolanatalia_oliveira@outlook.com>
2023-01-16 22:27:43 -0300

    cifrar minuscula funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -4,13 +4,15 @@ const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
 
 function encode(offset, mensagem) {
   let mensagemCriptografada = '';
+  const offsetNumber = parseInt(offset);
 
+  
   for (let i = 0; i < mensagem.length; i++) {
     const numeroPosicaoOriginal = alfabeto.indexOf(mensagem[i]); // p = 15
-    const novaPosicaoLetra = numeroPosicaoOriginal + offset; // s = 18
-
+    const novaPosicaoLetra = (numeroPosicaoOriginal + offsetNumber) % alfabeto.length; // s = 18
     mensagemCriptografada += alfabeto[novaPosicaoLetra];
   }
+
 
   return mensagemCriptografada;
 }
